60627b2a0b4a0fbe62f45f5fd62df5c31d1d527a
poemtree <yym1232@gmail.com>
2018-06-19 05:35:42 +0000

    creat, read 구현

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -7,5 +7,16 @@ class PostController < ApplicationController
   end
 
   def create
+    # 쌔로운 모델 객체를 만드는 방법...1
+    Post.create(title: params[:title], body: params[:body])
+    # 쌔로운 모델 객체를 만드는 방법...2
+    # post = Post.new
+    # post.title = params[:title]
+    # post.body = params[:body]
+    # post.save
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
